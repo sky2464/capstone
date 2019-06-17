7a6291d449a8f7c769c7faebb68241d18a95ada9
Alex-Cretella <51726063+Alex-Cretella@users.noreply.github.com>
2019-06-17 11:35:07 -0400

    Updated client.cpp
    added message encoding/decoding and event struct

--- client.cpp
+++ client.cpp
@@ -15,29 +15,67 @@
 
 using namespace std;
 
+int user_count = 0;
+
 struct event{
-	clock_t timestamp;
-	string message;
+	bool packet_type;//True for data packet/ False for control packet
+	int userid; //Local count of users
+	int packet_num; //Number of packets to send or Number of packet sent
+	clock_t timestamp;//Timestamp for message
+	bool checksum_valid;//Checksum correct
+	string message;//Data
 };
 
-string to_ascii(char c){
+vector<event> events;
+
+event decode_msg(string m){
+	event n;
+	time(&n.timestamp);
+	if(m[0] == "1"){//Decode data packet
+		n.packet_type = TRUE;
+		n.userid = user_count;
+		n.packet_num = atoi(m.substr(1,6).c_str());
+		if(checkSum(m.substr(10,8)) == m.substr(6, 4)){n.checksum_valid = TRUE;}
+		else{n.checksum_valid = TRUE;}
+		n.message = char(atoi(m.substr(10,8).c_str()));
+	}
+	if(m[0] == "0"){//Decode control packet
+		n.packet_type = TRUE;
+		n.userid = user_count;
+		n.packet_num = atoi(m.substr(1,6).c_str());
+		if(checkSum(m.substr(10,8)) == m.substr(6, 4)){n.checksum_valid = TRUE;}
+		else{n.checksum_valid = TRUE;}
+		n.message = "";
+	}
+	return n;
+}
+
+string ascii_to(string ascii){//Converts ascii binary string to a regular string
+	string msg = "";
+	for(int i = 0; i < ascii.length(); i + 8){
+		msg.append(char(atoi(ascii.substr(i, 8).c_str())));
+	}
+	return msg;
+}
+
+string to_ascii(char c){//Converts char to ascii binary string
 	int ascii = int(c);
 	string code = bitset<8>(ascii).to_string();
 	return code;
 }
 
-string ascii_string(string in){
+string ascii_string(string in){//Convert a string to an ascii binary string
 	string out = "";
 	for(int i = 0; i < in.length();i++){
-		out.append(in[i]);
+		out.append(to_ascii(in[i]));
 	}
 	return out;
 }
 
-string checkSum(string data){
+string checkSum(string data){//Checksum function
 	substr_a = atoi(code.substr(0, (code.length()/2)-1).c_str());
 	substr_b = atoi(code.substr((code.length()/2), (code.length()/2)).c_str());
-	int c = substr_a&substr_b;
+	int c = substr_a&substr_b;//XOR halves
 	return bitset<8>(c).to_string();
 }
 //Function to create a sequence of chunks to send
@@ -47,12 +85,12 @@ vector<string> header(string msg/*, int buff_size*/){
 	code = ascii_string(msg);
 	//Control message
 	string control = "0";
-	control.append(bitset<6>(code.length()).to_string());
+	control.append(bitset<6>(msg.length()).to_string());
 	control.append(bitset<12>(0).to_string());
 	msgs.push_back(control);
 	for(int i = 0; i < code.length(); i + 8){
 		string id = "1";
-		string data = code.substr(i, 7);
+		string data = code.substr(i, 8);
 		string chk = checkSum(data);
 		id.append(chk);
 		id.append(data);
@@ -84,7 +122,7 @@ int main(int argc, char const *argv[])
 
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
 	{ 
-		printf("\nConnection Failed \n"); 
+		printf("\nConnection Failed \n");
 		return -1; 
 	}
 	
