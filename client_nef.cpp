9020e5c972eb6658eddef2a6e8a59066a33c44ab
Alex-Cretella <51726063+Alex-Cretella@users.noreply.github.com>
2019-07-02 17:30:08 -0400

    Nefarious client/server
    Includes a menu to perform DOS actions

---
+++ client_nef.cpp
@@ -0,0 +1,151 @@
+// Client side C/C++ program to demonstrate Socket programming 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <bitset>
+#include <time.h>
+#include <math.h>
+
+#define PORT 8080 
+
+using namespace std;
+
+string zero_pad(int num, int len){//Pad a number with 0s
+    ostringstream ss;
+    ss << setw( len ) << setfill( '0' ) << num;
+    return ss.str();
+}
+
+string check_sum(string data){
+	int check_val = 256;//Arbitrary assigned value
+	int data_val = 0;
+	for(int i = 0; i < data.length(); i++){
+		data_val += int(data[i]);
+	}
+	//int xor_ = check_val&data_val;
+	return zero_pad(data_val, 4);//zero_pad(xor_, 4);
+}
+
+int hackerman_intf(){
+    int select;
+    cout << "[HACKERMAN INTERFACE]" << endl;
+    cout << "| 1 << Control DOS  |" << endl;//Send incorrect value for size of message in control packet
+    cout << "| 2 << Data DOS     |" << endl;//Dont send correct data packet repeatedly
+    cout << ">> ";
+    cin >> select;
+	return select; 
+}
+
+vector<string> data_packets(string data, int buffer_sz, int header_len){
+cout << "Creating packets..." << endl;
+
+	vector<string> pkt_list;
+	int data_chunk = buffer_sz - header_len;
+	for(int i = 0; i < (data.length()/data_chunk) + 1; i++){
+		string chunk = data.substr(data_chunk*i, data_chunk);
+		string pkt_num = zero_pad(i, 2);
+		string chk_sum = check_sum(chunk);
+		string pkt = "1" + pkt_num + chk_sum + chunk;
+		pkt_list.push_back(pkt);
+cout << "Pkt[" << i << "] " << pkt << endl;
+	}
+	return pkt_list;
+}
+
+string control_pkt(string data, int buffer_sz){
+	string p = "0";
+	string len = zero_pad(data.length(), 2);//max 99 characters message size for two character packet area
+	string sz = zero_pad(buffer_sz, 4);//Send buffer size to destination
+	p = p + len + sz;
+	return p;
+}
+
+int main(int argc, char const *argv[]) 
+{ 
+	int dos = hackerman_intf();
+
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	}
+	
+	//Client parameters
+	int header_len = 7;
+	int buffer_size = 10;
+
+	//Take user input message
+	string message;
+	string tempy;
+	cout << ">" ;
+	cin >> tempy;
+	getline(cin, message);
+	message.insert(0, tempy);
+	//Send client control packet
+	string cntrl = control_pkt(message, buffer_size);
+	if(dos == 1){cntrl[1] = '5';}//Nefarious option 1, give incorrect message size
+	const char *control = cntrl.c_str();
+	send(sock, control, strlen(control), 0);
+cout << "Control packet sent : " << cntrl << endl;
+	//Receive server control packet
+	char buffer[1024] = {0}; 
+	valread = read(sock, buffer, 1024);
+	//Decode server control packet
+	string serv_cntrl = string(buffer);
+cout << "Control packet received: " << serv_cntrl << endl;
+	//int expects = (atoi(serv_cntrl.substr(1, 2))/(buffer_size-header_len))+1;
+	int serv_buffer = atoi(serv_cntrl.substr(3, 4).c_str());
+	//Prepare data packets and send them
+	vector<string> data_pkts = data_packets(message, serv_buffer, header_len);
+cout << "<Packets created>" << endl;
+	vector<int> pkt_sent_cnt;
+	pkt_sent_cnt.reserve(data_pkts.size());
+	while(true){
+		int i = atoi(string(buffer).substr(1,2).c_str());
+		if(i == data_pkts.size()){break;}//If full message has already been sent, end connection
+cout << "Server request packet " << i;
+		if(dos == 2){i = 1;}//Nefarious option 2, client only sends the first packet no matter what is requested
+		string t = data_pkts[i];
+cout << " : " << t << endl;
+		pkt_sent_cnt[i] += 1;
+//t[5] = '2'; //Modify checksum to cause error
+//t[1] = '2'; //Modify packet number to cause error
+		if(pkt_sent_cnt[i] > 2){break;}//If server requests a packet more than twice, end connection
+		const char *temp = t.c_str();
+		send(sock, temp, strlen(temp), 0);
+cout << "Data packet sent : " << t << endl;
+		buffer[1024] = {0};
+		valread = read(sock, buffer, 1024);//Read request packet
+	}
+
+	// send(sock , hello , strlen(hello) , 0 ); 
+	// printf("Hello message sent\n"); 
+	// valread = read( sock , buffer, 1024); 
+	// printf("%s\n",buffer ); 
+	return 0; 
+} 
---
+++ server_nef.cpp
@@ -0,0 +1,173 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <bitset>
+#include <time.h>
+#include <math.h>
+
+#define PORT 8080 
+
+using namespace std;
+
+string zero_pad(int num, int len){//Pad a number with 0s
+    ostringstream ss;
+    ss << setw( len ) << setfill( '0' ) << num;
+    return ss.str();
+}
+
+string check_sum(string data){
+	int check_val = 256;//Arbitrary assigned value
+	int data_val = 0;
+	for(int i = 0; i < data.length(); i++){
+		data_val += int(data[i]);
+	}
+	//int xor_ = check_val&data_val;
+	return zero_pad(data_val, 4);//zero_pad(xor_, 4);
+}
+
+int hackerman_intf(){
+    int select;
+    cout << "[HACKERMAN INTERFACE]" << endl;
+    cout << "| 1 << Control DOS  |" << endl;//Dont send control packet to client
+    cout << "| 2 << Data DOS     |" << endl;//Request incorrect data packet (out of range)
+	cout << "| 3 << Request flood|" << endl;//Repeatedly request the same packet
+    cout << ">> ";
+    cin >> select; 
+return select;
+}
+
+vector<string> data_packets(string data, int buffer_sz, int header_len){
+	vector<string> pkt_list;
+	int data_chunk = buffer_sz - header_len;
+	for(int i = 0; i < (data.length()/data_chunk) + 1; i++){
+		string chunk = data.substr(data_chunk*i, data_chunk);
+		string pkt_num = zero_pad(i, 2);
+		string chk_sum = check_sum(chunk);
+		string pkt = "1" + pkt_num + chk_sum;
+		pkt_list.push_back(pkt);
+	}
+	return pkt_list;
+}
+
+string control_pkt(string data, int req_num, int buffer_sz){
+	string p = "0";
+	string len = zero_pad(req_num, 2);//packet number for server to request
+	string sz = zero_pad(buffer_sz, 4);//Send buffer size to destination
+	p = p + len + sz;
+	return p;
+}
+
+int main(int argc, char const *argv[]) 
+{ 
+	int dos = hackerman_intf();
+	int server_fd, new_socket, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	
+	// Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	// Forcefully attaching socket to the port 8080 
+	//if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+	{ 
+		//perror("setsockopt"); 
+		//exit(EXIT_FAILURE); 
+	} 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (bind(server_fd, (struct sockaddr *)&address, 
+								sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+while(true){
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+					(socklen_t*)&addrlen))<0) 
+	{ 
+		perror("accept"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+
+	//Server parameters
+	int header_len = 7;
+	int buffer_size = 15;
+	//Accept the client control packet
+	char buffer[1024] = {0}; 
+	valread = read(new_socket, buffer, 1024);
+	//Decode client control packet
+	string serv_cntrl = string(buffer);
+cout << "Control packet received: " << serv_cntrl << endl;
+	int expects = (atoi(serv_cntrl.substr(1, 2).c_str())/(buffer_size-header_len))+1;//Number of expected packets
+	int client_buffer = atoi(serv_cntrl.substr(3, 4).c_str());//Size of client buffer
+	//Send server control packet, requests first packet
+	if(dos == 1){break;}//Nefarious option 1, deny control packet handshake
+	string message = "";
+	string cntrl = control_pkt(message, 0, buffer_size);
+	const char *control = cntrl.c_str();
+	send(new_socket, control, strlen(control), 0);
+cout << "Control packet sent : " << cntrl << endl;
+	//Accept incoming data packets and confirm receiving
+	vector<string> pkt_data;
+	for(int i = 0; i < expects; i++){
+		char buffer1[1024] = {0};
+		while(string(buffer1).length() == 0){ 
+			valread = read(new_socket, buffer1, 1024);
+		}
+cout << "Data packet received: " << string(buffer1) << endl;
+		string chunk = string(buffer1).substr(7, buffer_size - header_len);
+		//Error checks here (packet number and checksum)
+		while(string(buffer1).substr(3,4) != check_sum(chunk) || atoi(string(buffer1).substr(1,2).c_str()) != i){
+			string s_a = control_pkt("", i, buffer_size);
+			send(new_socket, s_a.c_str(), strlen(s_a.c_str()), 0);
+			buffer1[1024] = {0}; 
+			valread = read(new_socket, buffer1, 1024);
+cout << "Data packet resent: " << string(buffer1) << endl;
+			chunk = string(buffer1).substr(7, buffer_size - header_len);
+		}
+		if(dos == 3){//Nefarious option 3, send packet request send again repeatedly
+			while(true){
+				string s_a = control_pkt("", i, buffer_size);
+				send(new_socket, s_a.c_str(), strlen(s_a.c_str()), 0);
+				buffer1[1024] = {0}; 
+				valread = read(new_socket, buffer1, 1024);
+	cout << "Data packet resent: " << string(buffer1) << endl;
+			}
+		}
+		pkt_data.push_back(chunk);
+		string cnfm = control_pkt("", i+1, buffer_size);//Request next packet
+		if(dos == 2){cnfm[1] = '9';}//Nefarious option 2, request invalid packet
+		send(new_socket, cnfm.c_str(), strlen(cnfm.c_str()), 0);//Send packet request
+	}
+	//Print the data
+	cout << "Message: ";
+	for(int i = 0; i < pkt_data.size(); i++){
+		cout << pkt_data[i];
+	}
+	cout << endl;
+}
+	return 0;
+} 
